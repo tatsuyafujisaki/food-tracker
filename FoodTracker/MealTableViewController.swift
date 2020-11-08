c811116afb46e52ab6f36053253686a4c77eba14
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-11-08 23:09:44 +0900

    Initial commit

---
+++ FoodTracker/MealTableViewController.swift
@@ -0,0 +1,177 @@
+import UIKit
+import os.log
+
+class MealTableViewController: UITableViewController {
+    
+    // MARK: Properties
+    var meals = [Meal]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        // Load any saved meals, otherwise load sample data.
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        } else {
+            // Load the sample data.
+            loadSampleMeals()
+        }
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Table view cells are reused and should be dequeued using a cell identifier
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+            fatalError("The dequeued cell is not instance of MealTableViewCell.")
+        }
+        
+        // Fetches the appropriate meal for the data source layout .
+        let meal = meals[indexPath.row]
+        
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+        
+        return cell
+    }
+    
+    // MARK: Actions
+    @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as? MealDetailViewController, let meal = sourceViewController.meal {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                // Update an existing meal.
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            } else {
+                // Add a new meal.
+                let newIndexPath = IndexPath(row: meals.count, section: 0)
+                
+                meals.append(meal)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+            // Save the meals.
+            saveMeals()
+        }
+    }
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            meals.remove(at: indexPath.row)
+            saveMeals()
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        switch segue.identifier ?? "" {
+        case "AddItem":
+            os_log("Adding a new meal.", log: .default, type: .debug)
+        case "ShowDetail":
+            guard let mealDetailViewController = segue.destination as? MealDetailViewController else {
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            
+            guard let selectedMealCell = sender as? MealTableViewCell else {
+                fatalError("Unexpected sender: \(String(describing: sender))")
+            }
+            
+            guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            let selectedMeal = meals[indexPath.row]
+            mealDetailViewController.meal = selectedMeal
+        default:
+            fatalError("Unexpected Segue Identifier; \(String(describing: segue.identifier))")
+        }
+    }
+    
+    // MARK: Private Methods
+    private func loadSampleMeals() {
+        let photo1 = #imageLiteral(resourceName: "meal1")
+        let photo2 = #imageLiteral(resourceName: "meal2")
+        let photo3 = #imageLiteral(resourceName: "meal3")
+        
+        guard let meal1 = Meal(name: "Hamburger", photo: photo1, rating: 4) else {
+            fatalError("Unable to instantiate meal1")
+        }
+        
+        guard let meal2 = Meal(name: "Sushi Bento", photo: photo2, rating: 5) else {
+            fatalError("Unable to instantiate meal2")
+        }
+        
+        guard let meal3 = Meal(name: "Grilled Beef", photo: photo3, rating: 3) else {
+            fatalError("Unable to instantiate meal3")
+        }
+        
+        meals += [meal1, meal2, meal3]
+    }
+    
+    private func saveMeals() {
+        guard let archivedData = try? NSKeyedArchiver.archivedData(withRootObject: meals, requiringSecureCoding: false) else {
+            fatalError("Unable to archive meals")
+        }
+        
+        do {
+            try archivedData.write(to: Meal.ArchiveURL)
+            os_log("Meals successfully saved.", log: OSLog.default, type: .debug)
+        } catch {
+            os_log("Failed to save meals...", log: OSLog.default, type: .error)
+        }
+    }
+    
+    private func loadMeals() -> [Meal]? {
+        do {
+            let data = try Data.init(contentsOf: Meal.ArchiveURL)
+            if let loadedData = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [Meal] {
+                os_log("Meals successfully loaded.", log: OSLog.default, type: .debug)
+                return loadedData
+            }
+        } catch {
+            os_log("Failed to load meals...", log: OSLog.default, type: .error)
+        }
+        return nil
+    }
+}
---
+++ FoodTracker/TableViewCell2.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell2.swift
+//  FoodTracker
+//
+//  Created by Tatsuya Fujisaki on 2020/11/07.
+//
+
+import UIKit
+
+class TableViewCell2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
